fa2b8caf3abcd63c4f178249c19edb869f56ced1
TaiYun <wendy0712000@gmail.com>
2024-08-23 10:35:10 +0800

    第一次測試

---
+++ AABB11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AABB11
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("YEE~");
+        }
+    }
+}
